a42247394d2a101d288e67cfb44d110b0aa0c50c
taiz <taiz7715@gmail.com>
2016-04-10 16:47:55 +0900

    fix tar option bug

--- spark/init.sh
+++ spark/init.sh
@@ -139,7 +139,7 @@ else
   esac
 
   echo "Unpacking Spark"
-  tar xvzf spark-*.tgz > /tmp/spark-ec2_spark.log
+  tar xvf spark-*.tgz > /tmp/spark-ec2_spark.log
   rm spark-*.tgz
   mv `ls -d spark-* | grep -v ec2` spark
 fi
